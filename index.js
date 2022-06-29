f21839ee295c90c21b539cf575ee646c6bca81cb
daragmas <daragmas@gmail.com>
2022-06-29 09:10:13 -0400

    commit tree 49abf26fb03ce779890d5eb86df7da6a6b44946f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHQInBlocks(){
+    let pickup = 10
+    if (pickup < 50){
+        return 50 - pickup;
+    }
+    else{
+        return pickup - 50;
+    }
+}
+
+function distanceFromHQInFeet(blocks){
+    return distanceFromHQInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(startTrip,endTrip){
+    if (startTrip > endTrip){return (startTrip-endTrip)*264}
+    else{return (endTrip-startTrip)*264}
+}
+
+function calculatesFarePrice(startTrip, endTrip){
+    let distance = distanceTravelledInFeet(startTrip,endTrip)
+    if (distance <= 400){return 0}
+    else if (distance<2000){return (distance-400)*.02}
+    else if (distance<2500){return 25}
+    else if (distance > 2500){return 'cannot travel that far'}
+}
